f9cda8c73448538a1a194c35e8e11e3f39c73def
boris <boris.fyot@gmail.com>
2019-04-08 14:43:20 +0200

    Améliroation du projet

--- projet5/script.sh
+++ projet5/script.sh
@@ -3,5 +3,9 @@
 #Il est constitué d'un script et n fichier de configuration                         #
 #Des options seront mise en place pour permettre son execution de manière           #
 #####################################################################################
+#										    #
+#										    #
+#										    #
+#####################################################################################
 
 echo "bonjour, bienvenue dans le chargement de votre configration"
